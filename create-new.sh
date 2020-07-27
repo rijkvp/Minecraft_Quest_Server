904489d7e80b315379014f0911014ca46e861315
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-07-27 17:21:40 +0200

    Perms?!

--- create-new.sh
+++ create-new.sh
@@ -1,6 +1,9 @@
 # Make sure the JRE is installed
 sudo apt install default-jre
 
+# Perms in this directory
+sudo chmod u+r+x *
+
 # Delete the old server copy
 ./delete-server.sh
 
@@ -10,4 +13,9 @@ cp -R server-template server-copy
 
 # Run the copied version
 cd ./server-copy
+
+# Give perms
+sudo chmod u+r+x *
+
+echo Starting server...
 ./start.sh
