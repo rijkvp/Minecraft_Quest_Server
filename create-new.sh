d8db81d4f817391d3a84b5f775d953ebf0e2d9a9
Youri-anet-a8 <59119671+Youri-anet-a8@users.noreply.github.com>
2020-08-01 14:48:07 +0200

    chunkdatapack

--- create-new.sh
+++ create-new.sh
@@ -1,3 +1,29 @@
+#Pre-load chunks
+if  [ $1 == 1 ]
+then
+echo "starting with pre-load chunks datapack"
+
+elif [ $1 = 2 ]
+then
+echo "Copying world from server-copy"
+cp -R -T ./server-copy/world/ chunkdatapack
+
+elif [ $1 = 3 ]
+then
+echo "pasting datapack from chunkdatapack"
+cp -R -T chunkdatapack ./server-copy/world/
+
+elif [ $1 == 0 ]
+then
+echo "Starting server with the McQuest Datapack"
+rm ./server-copy/world/datapack
+cd ./server-copy/world/
+mkdir datapack
+cd ..
+cd ..
+# Download the datapack
+./datapack.sh
+fi
 # Make sure the JRE is installed
 sudo apt install default-jre
 
@@ -10,9 +36,6 @@ sudo chmod u+r+x *
 # Create a copy of the template
 echo Copying templete...
 cp -R -T server-template server-copy
-
-# Download the datapack
-./datapack.sh
 
 # Go to the copied folder
 cd ./server-copy
