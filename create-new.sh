ee1dc1b46137878d4d4f7f6792bb299d66a9cb98
Youri-anet-a8 <59119671+Youri-anet-a8@users.noreply.github.com>
2020-08-04 19:17:07 +0200

    Version/Plugin selector

--- create-new.sh
+++ create-new.sh
@@ -1,12 +1,8 @@
 # Perms in this directory
 sudo chmod u+r+x *
 
-# Download the datapack
-./datapack.sh
-
 # Make sure the JRE is installed
 sudo apt install default-jre
-
 
 # Delete the old server copy
 ./delete-server.sh
@@ -23,3 +19,31 @@ sudo chmod u+r+x *
 
 echo Starting server...
 ./start.sh
+
+#Vannila/Paper
+if  [ $1 == 0 ]
+then
+echo "starting with Vannila"
+./start.sh 0 #Vanilla
+./datapack.sh #Copying the newest Datapack
+rm ./server-template/plugins/ViaVersion-3.0.1.jar
+rm ./server-template/plugins/ViaBackwards-3.0.1.jar
+rm ./server-template/plugins/ViaRewind-1.5.0.jar
+
+elif [ $1 == 1 ]
+then
+echo "starting with Paper and SuperVanish"
+rm ./server-template/plugins/ViaVersion-3.0.1.jar
+rm ./server-template/plugins/ViaBackwards-3.0.1.jar
+rm ./server-template/plugins/ViaRewind-1.5.0.jar
+./start.sh 1 #Paper
+./datapack.sh #Copying the newest Datapack
+
+elif [ $1 == 2 ]
+then
+echo "starting with Paper and Viaversion & SuperVanish"
+cp -R -T ./server-template/viaversion ./server-template/plugins
+./start.sh 1 #Paper
+./datapack.sh #Copying the newest Datapack
+fi
+
--- server-template/start.sh
+++ server-template/start.sh
@@ -1,1 +1,10 @@
-java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar server.jar nogui
+if  [ $1 == 0 ]
+then
+echo "starting with Vannila"
+java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar vannila.jar nogui
+
+elif [ $1 = 1 ]
+then
+echo "starting with Paper"
+java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar paper.jar nogui
+fi
