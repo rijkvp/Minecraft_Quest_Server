7b996b86eb8c8f4cca986f894f1d4f89cd7c71ae
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-08-05 15:38:05 +0200

    Delete all the SH*T and use fabric

--- create-new.sh
+++ create-new.sh
@@ -1,8 +1,8 @@
-# Perms in this directory
-sudo chmod u+r+x *
-
 # Make sure the JRE is installed
 sudo apt install default-jre
+
+# Perms in this directory
+sudo chmod u+r+x *
 
 # Delete the old server copy
 ./delete-server.sh
@@ -10,6 +10,9 @@ sudo apt install default-jre
 # Create a copy of the template
 echo Copying templete...
 cp -R -T server-template server-copy
+
+# Download the datapack
+./datapack.sh
 
 # Go to the copied folder
 cd ./server-copy
@@ -19,31 +22,3 @@ sudo chmod u+r+x *
 
 echo Starting server...
 ./start.sh
-
-#Vannila/Paper
-if  [ $1 == 0 ]
-then
-echo "starting with Vannila"
-./start.sh 0 #Vanilla
-./datapack.sh #Copying the newest Datapack
-rm ./server-template/plugins/ViaVersion-3.0.1.jar
-rm ./server-template/plugins/ViaBackwards-3.0.1.jar
-rm ./server-template/plugins/ViaRewind-1.5.0.jar
-
-elif [ $1 == 1 ]
-then
-echo "starting with Paper and SuperVanish"
-rm ./server-template/plugins/ViaVersion-3.0.1.jar
-rm ./server-template/plugins/ViaBackwards-3.0.1.jar
-rm ./server-template/plugins/ViaRewind-1.5.0.jar
-./start.sh 1 #Paper
-./datapack.sh #Copying the newest Datapack
-
-elif [ $1 == 2 ]
-then
-echo "starting with Paper and Viaversion & SuperVanish"
-cp -R -T ./server-template/viaversion ./server-template/plugins
-./start.sh 1 #Paper
-./datapack.sh #Copying the newest Datapack
-fi
-
