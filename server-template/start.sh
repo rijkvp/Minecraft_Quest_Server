8a45cd734b36c3a978e3def9480e37eac24863e5
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-08-05 16:18:39 +0200

    Update start script

--- server-template/start.sh
+++ server-template/start.sh
@@ -1,10 +1,1 @@
-if  [ $1 == 0 ]
-then
-echo "starting with Vannila"
-java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar vannila.jar nogui
-
-elif [ $1 = 1 ]
-then
-echo "starting with Paper"
-java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar paper.jar nogui
-fi
+java -server -Xms4096M -Xmx32768M -XX:ParallelGCThreads=8 -jar server.jar nogui
